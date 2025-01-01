d43666d492aee9efd26cc1f573327d280ac940ee
Wolfram <129839889+Wolfram49@users.noreply.github.com>
2025-01-01 12:53:02 +0200

    Westerland characters DONE

--- common/defines/00_lotr_defines.lua
+++ common/defines/00_lotr_defines.lua
@@ -11,3 +11,12 @@ NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 1		-- Max height of support in 
 
 -- Food consumption etc.
 NDefines.NMilitary.ARMY_IDLE_FUEL_MULT = 0.05						-- fuel consumption ratio while just existing
+
+-- Advisor and general changes
+NDefines.NCharacter.OFFICER_CORP_ADVISOR_ENTRIES_IN_MENU = { "high_command", "theorist", "army_chief", "cavalry_chief", "navy_chief" }
+NDefines.NCharacter.OFFICER_CORP_HIGH_COMMAND_SLOTS_IN_MENU = 2 --For Alert manager to count the number of High Command Slots in the UI
+NDefines.NCharacter.POLITICAL_ADVISOR_SLOTS_IN_MENU = 2 --For Alert manager to count the number of Political Advisor Slots in the UI
+NDefines.NCharacter.DEFAULT_PP_COST_FOR_MILITARY_ADVISOR = 150	-- When an advisor does not have cost assigned this is the default used	
+NDefines.NCharacter.SPECIALIST_ADVISOR_MIN_RANK = 3
+NDefines.NCharacter.EXPERT_ADVISOR_MIN_RANK = 5
+NDefines.NCharacter.GENIUS_ADVISOR_MIN_RANK = 7
